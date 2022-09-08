b7c62f284f1e639577ab8a9e502483ce382a6c09
Lewis <lewismunyambu@gmail.com>
2022-09-08 21:28:23 +0300

    completed task

---
+++ index.js
@@ -0,0 +1,38 @@
+class Account  {
+    constructor(Id = "", data = []) {
+        this.timestamp = timestamp;
+        this.data = data;
+        this. balance = balance;
+        this. keypair = new keypair;
+        this.update  = new balance;
+        this.hash = this.getHash();
+        this.prevHash = ""; 
+        this.nonce = 0;
+    }
+
+}
+const balance = () =>{
+    console.log(`Initial balance ${add } to get current balance else subtract to get current balance`);
+}
+
+const Account = (implement) =>{
+        let keypair = KeySignew();
+        let public_key = hexencode
+        keypair.get_public_key
+}
+const  keypair = (add) =>{
+keysig.selfwallet.push
+}
+const update = (balance) =>{
+self.balance
+}
+const payment = (create) =>{
+    
+}
+
+const print = (balance) =>{
+    console.log("Balance: {}", self.balance);
+}
+const sign = (data) =>{
+    console.log(Id)
+}
